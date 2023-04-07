8dd39baa812ef2a777451d44562c58b68ae29661
tasmim-concept <amina.seraoui@outlook.fr>
2023-04-07 09:10:05 +0200

    :sparkles: menu burger

---
+++ assets/controllers/menu_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    connect() {
+        const toggle = this.element.querySelector('.toggle-menu')
+
+        toggle.addEventListener('click', () => {
+            this.element.classList.toggle('active')
+        })
+    }
+}
