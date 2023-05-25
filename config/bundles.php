932e64c3a7f4b881d7c10948156ecbf8d93eda2a
tasmim-concept <amina.seraoui@outlook.fr>
2023-05-25 22:23:51 +0200

    :heavy_minus_sign: remove fckeditor

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
